f5f7de4bf71365a93c8020aa6c1af9aa75fb6636
Hitesh <hp212212212@gmail.com>
2023-02-27 02:43:12 -0700

    Initial committed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, lazy, useEffect } from "react";
+import React, { createContext, useEffect } from "react";
 import Header from "./Components/Header";
 import { Routes, Route } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -7,20 +7,12 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import Registration from "./Components/Registration";
 import UserLogin from "./Components/UserLogin";
 import Admin from "./Components/Admin";
-import Admin1 from "./Components/Admin1";
-import AdminPortal from "./Components/AdminPortal";
 import Books from "./Components/Books";
 import Home from "./Components/Home";
-import AboutUs from "./Components/AboutUs";
 import { ActiveAdmin, CurrentTitleList } from "./Context";
 import { useState } from "react";
 import { GetBookTitle } from "./Server/GETAxios";
 import FlipPages from "./Components/FlipPages";
-// const Home = lazy(() => import('./Components/Home'));
-// const Admin = lazy(() => import('./Components/Admin'));
-// const UserLogin = lazy(() => import('./Components/UserLogin'));
-// const Registration = lazy(() => import('./Components/Registration'));
-// const AdminPortal = lazy(() => import('./Components/AdminPortal'));
 export const ActiveLogin = createContext()
 
 function App() {
@@ -44,10 +36,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="UserLogin" element={<UserLogin />} />
               <Route path="Registration" element={<Registration />} />
-              <Route path="AdminPortal" element={<AdminPortal />} />
               <Route path="Admin" element={<Admin />} />
-              <Route path="Admin1" element={<Admin1 />} />
-              <Route path="AboutUs" element={<AboutUs />} />
               <Route path="Books" element={<Books />} />
               <Route path="/Books/FlipPages" element={<FlipPages />} />
             </Routes>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,9 +14,6 @@ function Header() {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="AboutUs">About Us</NavLink>
-              </li>
-              <li class="nav-item">
                 <NavLink className="nav-link active" aria-current="page" to="Books">Books</NavLink>
               </li>
               <li class="nav-item">
@@ -24,19 +21,6 @@ function Header() {
               </li>
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                {/* <a class="nav-link" style={{ color: "black" }} href="/a">Contact Us</a> */}
-                <div class="dropdown">
-                  <button class="btn dropdown-toggle btn-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    ABC
-                  </button>
-                  <ul class="dropdown-menu">
-                    <li><NavLink className="dropdown-item" to="Admin1">Admin1</NavLink></li>
-                    <li><NavLink className="dropdown-item" to="Admin1/UserLogin1">Users1</NavLink></li>
-                    <li><NavLink className="dropdown-item" to="Admin1/Registration1">New Registrations1</NavLink></li>
-                  </ul>
-                </div>
-              </li>
               <li class="nav-item">
                 {/* <NavLink className="nav-link" to="Login">Login</NavLink> */}
                 <div class="dropdown">
@@ -52,7 +36,7 @@ function Header() {
               </li>
               <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-warning" type="submit">Search</button>
+                <button class="btn btn-outline-warning" type="button">Search</button>
               </form>
             </ul>
           </div>
